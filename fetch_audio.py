ff6d0e8adf69e28a5dcc1f48a982ffd134317b39
Ethan Hill <ethan.hill1999@outlook.com>
2019-10-18 17:51:49 -0500

    All the files

--- fetch_audio.py
+++ fetch_audio.py
@@ -1,15 +1,17 @@
 import os, sqlite3, csv
 from gtts import gTTS
 
+print()
+
 conn = sqlite3.connect('master_dict.db')
 c = conn.cursor()
-c.execute("select distinct kana from dictionary;")
+c.execute("select distinct english, id from dictionary;")
 
 for word in c.fetchall():
-    my_path = str('./audio/ja/' + word[0].replace('\n','') + '.mp3')
+    my_path = str('./audio/en/' + str(word[1]) + '.mp3')
     if not os.path.exists(my_path):
         try:
-            tts = gTTS(text=word[0], lang='ja')
+            tts = gTTS(text=word[0], lang='en')
             tts.save(my_path)
         except: pass
 
