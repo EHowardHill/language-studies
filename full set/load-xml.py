58964caa8b880d617fe8464cff79449044d1455c
Ethan Hill <ethan.hill1999@outlook.com>
2019-12-19 16:55:42 -0600

    LEGIT DICTIONARY, FINALLY!

--- full set/load-xml.py
+++ full set/load-xml.py
@@ -1,7 +1,7 @@
 import os, sqlite3, pprint, xmltodict, json
 from tinydb import TinyDB, Query
 
-conn = sqlite3.connect('jisho.db')
+conn = sqlite3.connect('dict.db')
 c = conn.cursor()
 
 # Build a basic dictionary
@@ -80,9 +80,87 @@ def retbit(t,bit):
     else:
         return None
 
-c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(c.fetchall())
+def pretty(x):
+    if len(x) == 1:
+        return '"' + x[0][1].replace('"','\'') + '"'
+    elif len(x) == 0:
+        return 'NULL'
+    else:
+        return '"' + json.dumps([y[1] for y in x],ensure_ascii=False).replace("\"","'") + '"'
 
+perc = 0
+size = len(full_data)
+for bit in range(size):
+    for o in range(0,5):
+        if 'sense_' + str(o) in full_data[bit]:
+
+            # Watch progress
+            if int((bit / size) * 100) > perc:
+                perc = int((bit / size) * 100)
+                print(str(perc) + "%")
+
+            t = [tt for tt in full_data[bit]['sense_' + str(o)] if tt != []]
+            query = (
+                'insert or ignore into sense (word_id, pos, expl, gloss, xref, misc, s_inf, dial, lit, field, stagr, ant, stagk, fig) values (' +
+                str(bit) + ', ' +
+                pretty([tt for tt in t if tt[0] == 'pos']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'gloss g_type="expl"']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'gloss']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'xref']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'misc']) + ", " +
+                pretty([tt for tt in t if tt[0] == 's_inf']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'dial']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'gloss g_type="lit"']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'field']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'stagr']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'ant']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'stagk']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'gloss g_type="fig"']) +
+                ');'
+            )
+            c.execute(query)
+            conn.commit()
+
+"""
+size = len(full_data)
+for bit in range(size):
+    for o in range(0,1):
+        if 'k_ele_' + str(o) in full_data[bit]:
+            print (str(bit) + " / " + str(size))
+            t = [tt for tt in full_data[bit]['k_ele_' + str(o)] if tt != []]
+            query = (
+                'insert or ignore into k_ele (word_id, keb, ke_pri, ke_inf) values (' +
+                str(bit) + ', ' +
+                pretty([tt for tt in t if tt[0] == 'keb']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'ke_pri']) + ", " +
+                pretty([tt for tt in t if tt[0] == 'ke_inf']) +
+                ');'
+            )
+            c.execute(query)
+            conn.commit()
+            """
+
+"""
+size = len(full_data)
+for bit in range(size):
+    for o in range(0,1):
+        if 'r_ele_' + str(o) in full_data[bit]:
+            print (str(bit) + " / " + str(size))
+            t = [tt for tt in full_data[bit]['r_ele_0'] if tt != []]
+            query = (
+                'insert or ignore into r_ele (word_id, reb, re_pri, re_restr, re_inf) values (' +
+                str(bit) + ', ' +
+                pretty([tt for tt in t if tt[0] == 'reb']) + ", " +
+                pretty([tt for tt in t if tt[0] == 're_pri']) + ", " +
+                pretty([tt for tt in t if tt[0] == 're_restr']) + ", " +
+                pretty([tt for tt in t if tt[0] == 're_inf']) +
+                ');'
+            )
+            c.execute(query)
+            conn.commit()
+"""
+
+"""
 size = len(full_data)
 for bit in range(size):
     print(str(bit) + " / " + str(size))
@@ -103,5 +181,6 @@ for bit in range(size):
             retbit(t,'k_ele_2'))
         )
     conn.commit()
+"""
 
 conn.close()
